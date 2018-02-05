1668ed0eee9a5c011d775651b3be9115f9af75ec
jcrowder7 <35549414+jcrowder7@users.noreply.github.com>
2018-02-05 14:09:00 -0600

    Verse and Chorus

---
+++ VerseAndChorus.java
@@ -0,0 +1,36 @@
+package com.jetbrains;
+
+public class VerseAndChorus {
+
+    public static void Chorus()
+    {
+        System.out.println("With a knick-knack paddywhack,\nGive the dog a bone, \nThis old man came rolling home.");
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("This old man, he played one. \nHe played knick-knack on my thumb;");
+        Chorus();
+        System.out.println();
+
+        System.out.println("This old man, he played two. \nHe played knick-knack on my shoe;");
+        Chorus();
+        System.out.println();
+
+        System.out.println("This old man, he played three. \nHe played knick-knack on my knee;");
+        Chorus();
+        System.out.println();
+
+        System.out.println("This old man, he played four. \nHe played knick-knack on my door;");
+        Chorus();
+        System.out.println();
+
+        System.out.println("This old man, he played five. \nHe played knick-knack on my hive;");
+        Chorus();
+        System.out.println();
+
+        System.out.println("This old man, he played aiz. \nHe played knick-knack on my sticks;");
+        Chorus();
+        System.out.println();
+    }
+}
